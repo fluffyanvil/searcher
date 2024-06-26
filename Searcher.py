3d25d67b76fe356ed6c6442a015cb5ea8190c6f5
Гарнов Сергей Александрович <SGarnov@alfabank.ru>
2024-06-26 22:41:03 +0300

    1) index column 2) output filename

--- Searcher.py
+++ Searcher.py
@@ -23,17 +23,17 @@ column_to_compare = 4
 # populate WBS with WON2SAP data
 start_time = time.time()
 with open(path_new, encoding="utf-8") as newFile:
-    new_csv_reader = csv.DictReader(newFile, delimiter='|')
+    new_csv_reader = csv.reader(newFile, delimiter='|')
     now = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
-    filename = os.path.join(folder, f'{now}.output.csv')
+    filename = os.path.join(folder, f'NEW_wbs_{now}.csv')
     
     modified_input = filename;
     with open(filename, 'w', newline='', encoding="utf-8") as outfile:
 
-        output_csv_writer = csv.DictWriter(outfile, delimiter='|', fieldnames=None)
+        output_csv_writer = csv.writer(outfile, delimiter='|')
 
         with open(path_old, encoding="utf-8") as oldFile:      
-            old_csv_reader = csv.DictReader(oldFile, delimiter='|')
+            old_csv_reader = csv.reader(oldFile, delimiter='|')
 
             rows_old = list(old_csv_reader)
             rows_new = list(new_csv_reader)
@@ -44,10 +44,11 @@ with open(path_new, encoding="utf-8") as newFile:
             data = []
 # fill index for full match (ProjectNumber + ResellerCode)
             for rowfd in rows_old:
-                old_index[list(rowfd.values())[column_to_compare]] = rowfd
+                old_index[rowfd[column_to_compare]] = rowfd
                 
-            for rowfd in rows_new:
-                new_index[list(rowfd.values())[column_to_compare]] = rowfd
+            for idx, rowfd in enumerate(rows_new):
+                rowfd.append(idx+1)
+                new_index[rowfd[column_to_compare]] = rowfd
                 
             new_rows = list(new_index.keys())
             old_rows = list(old_index.keys())
@@ -55,12 +56,17 @@ with open(path_new, encoding="utf-8") as newFile:
             excepted = list(set(new_rows) - set(old_rows))
                 
 # counters for fillings
+
+            targetRows = []           
             
             for key in excepted:
-
-                newrow = new_index[key]      
-                output_csv_writer.fieldnames = list(newrow.keys())
-                output_csv_writer.writerow(newrow)
+                newrow = new_index[key]
+                targetRows.append(newrow)
+                
+            new_list = sorted(targetRows, key=lambda x: x[len(x)-1], reverse=False)
+            
+            if len(targetRows) > 0:           
+                output_csv_writer.writerows(new_list)
 
 
 print("--- NEW FILE ROWS = %s, OLD FILE ROWS = %s, EXCEPT = %s ---" % (len(new_rows), len(old_rows), len(excepted)))
